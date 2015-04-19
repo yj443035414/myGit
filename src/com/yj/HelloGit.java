e6ad334f0af0fa063d35ad5fcde863e60f176ad7
yj443035414 <yj443035414@gmail.com>
2015-04-19 16:29:25 +0800

    dsafd
    sdfsdf

--- src/com/yj/HelloGit.java
+++ src/com/yj/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 
-		System.out.println("First Git Project! Hello Git!");
+		System.out.println("First Git Project! Hello Git!AAAA");
 	}
 
 }
