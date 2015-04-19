1913cdf00ff31715d0d2bb3017f7a2e48b1dd308
yj443035414 <yj443035414@gmail.com>
2015-04-19 15:07:42 +0800

    Add print a statement

--- src/com/yj/HelloGit.java
+++ src/com/yj/HelloGit.java
@@ -3,7 +3,7 @@ package com.yj;
 public class HelloGit {
 
 	public static void main(String[] args) {
-
+		System.out.println("First Git Project! Hello Git!");
 		System.out.println("First Git Project! Hello Git!");
 	}
 
